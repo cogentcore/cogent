fb1e21330fe364254088b9bc30ae3491c7f62889
rohrlich <john.rohrlich@colorado.edu>
2018-11-08 09:12:09 -0700

    Completion and spell code moved to giv

--- gide.go
+++ gide.go
@@ -13,7 +13,6 @@ package gide
 import (
 	"bytes"
 	"fmt"
-	"go/token"
 	"html"
 	"log"
 	"net/url"
@@ -26,11 +25,9 @@ import (
 	"time"
 
 	"github.com/goki/gi"
-	"github.com/goki/gi/complete"
 	"github.com/goki/gi/giv"
 	"github.com/goki/gi/oswin"
 	"github.com/goki/gi/oswin/key"
-	"github.com/goki/gi/spell"
 	"github.com/goki/gi/units"
 	"github.com/goki/ki"
 	"github.com/goki/ki/kit"
@@ -696,13 +693,16 @@ func (ge *Gide) ViewFileNode(tv *giv.TextView, vidx int, fn *giv.FileNode) {
 			switch ln {
 			case "Go":
 				if ge.Prefs.Editor.Completion {
-					tv.SetCompleter(tv, CompleteGo, CompleteGoEdit)
-				}
-			case "Markdown":
-				if ge.Prefs.Editor.SpellCorrect {
-					tv.SetSpellCorrect(tv, SpellCorrectEdit)
+					tv.SetCompleter(tv, giv.CompleteGo, giv.CompleteGoEdit)
 				}
 			default:
+				//if ge.Prefs.Editor.Completion {
+				//	tv.SetCompleter(tv, giv.CompleteText, giv.CompleteTextEdit)
+				//}
+			case "Markdown":
+				if ge.Prefs.Editor.SpellCorrect {
+					tv.SetSpellCorrect(tv, giv.SpellCorrectEdit)
+				}
 				//tv.SetCompleter(tv, CompleteText, CompleteTextEdit)
 			}
 		} else {
@@ -2977,72 +2977,4 @@ func NewGideWindow(path, projnm string, doPath bool) (*gi.Window, *Gide) {
 	defer cmd.Run()
 
 	return win, ge
-}
-
-// CompleteGocode uses github.com/mdempsky/gocode to do code completion
-// gocode requires the entire file and the position of the cursor within the file
-func CompleteGo(data interface{}, text string, pos token.Position) (matches complete.Completions, seed string) {
-	var txbuf *giv.TextBuf
-	switch t := data.(type) {
-	case *giv.TextView:
-		txbuf = t.Buf
-	}
-	if txbuf == nil {
-		log.Printf("complete.Complete: txbuf is nil - can't complete\n")
-		return
-	}
-
-	seed = complete.SeedGolang(text)
-	textbytes := make([]byte, 0, txbuf.NLines*40)
-	for _, lr := range txbuf.Lines {
-		textbytes = append(textbytes, []byte(string(lr))...)
-		textbytes = append(textbytes, '\n')
-	}
-	results := complete.CompleteGo(textbytes, pos)
-	//matches = complete.MatchSeedCompletion(results, seed)
-	//return matches, seed
-	return results, seed
-}
-
-// CompleteGoEdit uses the selected completion to edit the text
-func CompleteGoEdit(data interface{}, text string, cursorPos int, selection string, seed string) (s string, delta int) {
-	s, delta = complete.EditGoCode(text, cursorPos, selection, seed)
-	return s, delta
-}
-
-// CompleteText does completion for text files
-func CompleteText(data interface{}, text string, pos token.Position) (matches complete.Completions, seed string) {
-	err := gi.InitSpell() // text completion uses the spell code to generate completions and suggestions
-	if err != nil {
-		fmt.Println("Could not initialize spelling model: Spelling model needed for text completion: %v", err)
-		return nil, seed
-	}
-
-	seed = complete.SeedWhiteSpace(text)
-	if seed == "" {
-		return nil, seed
-	}
-	result, err := complete.CompleteText(seed)
-	if err != nil {
-		fmt.Println("Error completing text: %v", err)
-		return nil, seed
-	}
-	possibles := complete.MatchSeedString(result, seed)
-	for _, p := range possibles {
-		m := complete.Completion{Text: p, Icon: ""}
-		matches = append(matches, m)
-	}
-	return matches, seed
-}
-
-// CompleteTextEdit uses the selected completion to edit the text
-func CompleteTextEdit(data interface{}, text string, cursorPos int, selection string, seed string) (s string, delta int) {
-	s, delta = complete.EditText(text, cursorPos, selection, seed)
-	return s, delta
-}
-
-// SpellCorrectEdit uses the selected correction to edit the text
-func SpellCorrectEdit(data interface{}, text string, cursorPos int, new string, old string) (s string, delta int) {
-	s, delta = spell.CorrectText(text, cursorPos, old, new)
-	return s, delta
 }
