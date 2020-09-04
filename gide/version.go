83238e7e5a5938d4b1ee9909d032c1b199d6f7ed
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-09-04 14:56:25 -0700

    v1.0.8 release -- version.go updated

--- gide/version.go
+++ gide/version.go
@@ -3,8 +3,8 @@
 package gide
 
 const (
-	Version     = "v1.0.7"
-	GitCommit   = "8d83a42" // the commit JUST BEFORE the release
-	VersionDate = "2020-08-23 19:57" // UTC
+	Version     = "v1.0.8"
+	GitCommit   = "971d81e" // the commit JUST BEFORE the release
+	VersionDate = "2020-09-04 21:56" // UTC
 )
 
