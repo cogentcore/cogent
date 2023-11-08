98825d3bc8cb154a151cbaa735f6ff5092f9c891
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-08 11:15:25 -0800

    improved ExtractText

--- gidom/text.go
+++ gidom/text.go
@@ -10,18 +10,27 @@ import (
 	"golang.org/x/net/html"
 )
 
-// ExtractText recursively extracts all of the text from the given [*html.Node],
-// adding any appropriate inline markup for formatted text.
+// ExtractText recursively extracts all of the text from the children
+// of the given [*html.Node], adding any appropriate inline markup for
+// formatted text. It should not be called on text nodes themselves;
+// for that, you can directly access the [html.Node.Data] field.
 func ExtractText(n *html.Node) string {
+	if n.FirstChild == nil {
+		return ""
+	}
+	return extractTextImpl(n.FirstChild)
+}
+
+func extractTextImpl(n *html.Node) string {
+	str := ""
 	if n.Type == html.TextNode {
-		str := strings.TrimSpace(n.Data)
-		if n.NextSibling != nil {
-			str += ExtractText(n.NextSibling)
-		}
-		return str
+		str += strings.TrimSpace(n.Data)
 	}
 	if n.FirstChild != nil {
-		return ExtractText(n.FirstChild)
+		str += extractTextImpl(n.FirstChild)
 	}
-	return ""
+	if n.NextSibling != nil {
+		str += " " + extractTextImpl(n.NextSibling)
+	}
+	return str
 }
