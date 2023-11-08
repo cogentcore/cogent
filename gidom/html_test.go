7651bbb3e547747acc4ee3e3b8a90db53dc795c4
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-07 20:13:01 -0800

    added basic render test

---
+++ gidom/html_test.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2023, The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build offscreen
+
+package gidom
+
+import (
+	"testing"
+
+	"goki.dev/gi/v2/gi"
+	"goki.dev/gi/v2/gimain"
+	"goki.dev/goosi"
+)
+
+func TestRenderHTML(t *testing.T) {
+	gimain.Run(func() {
+		sc := gi.NewScene("test-render-html")
+
+		s := `Hello, world!`
+		err := ReadHTMLString(sc, s)
+		if err != nil {
+			t.Error(err)
+		}
+
+		gi.NewWindow(sc).Run()
+
+		goosi.CaptureAs("test-render-html.png")
+	})
+}
