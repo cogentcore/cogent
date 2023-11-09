457c82d09fac8126d820c30c15a2393cb76dbfec
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-08 21:03:37 -0800

    added print statement

--- gidom/md.go
+++ gidom/md.go
@@ -20,6 +20,7 @@ func ReadMD(par gi.Widget, b []byte) error {
 	if err != nil {
 		return fmt.Errorf("error parsing MD (markdown): %w", err)
 	}
+	fmt.Println(buf.String())
 	return ReadHTML(par, &buf)
 }
 
