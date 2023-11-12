a82a24e749d4c9200cd45e17b55e309d0d83447f
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-11 21:25:20 -0800

    call StyleFromProp with node styles

--- gidom/style.go
+++ gidom/style.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 
 	"goki.dev/gi/v2/gi"
+	"goki.dev/girl/styles"
 	"golang.org/x/net/html"
 )
 
@@ -22,6 +23,12 @@ func ApplyStyle(ctx Context, par gi.Widget, n *html.Node) error {
 		for _, match := range matches {
 			w := ctx.WidgetForNode(match)
 			fmt.Println("STYLE", w, ":\n", r)
+			w.Style(func(s *styles.Style) {
+				for _, decl := range r.Declarations {
+					// TODO(kai/styprops): parent style and context
+					s.StyleFromProp(s, decl.Property, decl.Value, nil)
+				}
+			})
 		}
 	}
 	return nil
