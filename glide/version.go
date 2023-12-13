f29fa9f32e22d7e50b2c4fe36b09ea83f92b4fb7
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 16:30:38 -0800

    updated version to v0.0.8

--- glide/version.go
+++ glide/version.go
@@ -4,9 +4,9 @@ package glide
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.0.7"
+	Version = "v0.0.8"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "afeff7c"
+	GitCommit = "7ac8d1c"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 00:12"
+	VersionDate = "2023-12-13 00:30"
 )
