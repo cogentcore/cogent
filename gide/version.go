d4314858723ad7e390ff5cf9dd6726d2674c9b6d
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 17:03:35 -0800

    updated version to v2.0.0-dev0.0.11

--- gide/version.go
+++ gide/version.go
@@ -4,9 +4,9 @@ package gide
 
 const (
 	// Version is the version of this package being used
-	Version = "v2.0.0-dev0.0.10"
+	Version = "v2.0.0-dev0.0.11"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "fd6295f"
+	GitCommit = "6353ce4"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-13 00:30"
+	VersionDate = "2023-12-13 01:03"
 )
