1a4a88047e060ac1aa93deaf3f17e5839ebe3ced
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-19 11:30:49 -0800

    started files app

---
+++ files.go
@@ -0,0 +1,19 @@
+// Copyright 2023 The GoKi Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"goki.dev/gi/v2/gi"
+	"goki.dev/gi/v2/gimain"
+	"goki.dev/gi/v2/giv"
+)
+
+func main() { gimain.Run(app) }
+
+func app() {
+	b := gi.NewAppBody("files")
+	giv.NewFileView(b)
+	b.NewWindow().Run().Wait()
+}
