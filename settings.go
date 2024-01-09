d937fdab4694bad6843802ce851280beb45bfb42
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-08 22:01:02 -0800

    removed gimain usage

--- settings.go
+++ settings.go
@@ -6,13 +6,10 @@ package main
 
 import (
 	"goki.dev/gi/v2/gi"
-	"goki.dev/gi/v2/gimain"
 	"goki.dev/gi/v2/giv"
 )
 
-func main() { gimain.Run(app) }
-
-func app() {
+func main() {
 	b := gi.NewAppBody("goki-settings")
 	giv.SettingsView(b)
 	b.NewWindow().Run().Wait()
