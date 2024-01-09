221ff4aaaf75674291ad31e408d9e0eaa85751eb
Kai O'Reilly <koreilly5297@gmail.com>
2024-01-08 22:11:49 -0800

    removed gimain usage

--- gidom/html_test.go
+++ gidom/html_test.go
@@ -8,21 +8,18 @@ import (
 	"testing"
 
 	"goki.dev/gi/v2/gi"
-	"goki.dev/gi/v2/gimain"
 )
 
 func TestRenderHTML(t *testing.T) {
-	gimain.Run(func() {
-		sc := gi.NewScene("test-render-html")
-		s := `
+	sc := gi.NewScene("test-render-html")
+	s := `
 		<h1>Gidom</h1>
 		<p>This is a demonstration of the various features of gidom</p>
 		<button>Hello, world!</button>
 		`
-		err := ReadHTMLString(BaseContext(), sc, s)
-		if err != nil {
-			t.Error(err)
-		}
-		sc.AssertPixelsOnShow(t, "test-render-html.png")
-	})
+	err := ReadHTMLString(BaseContext(), sc, s)
+	if err != nil {
+		t.Error(err)
+	}
+	sc.AssertPixelsOnShow(t, "test-render-html.png")
 }
