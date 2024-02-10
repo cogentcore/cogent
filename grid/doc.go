e4b45da58092ce7b2063c3155d84dd286aad5c6c
Randall C. O'Reilly <oreilly@ucdavis.edu>
2024-02-10 12:19:55 -0800

    add grid files

--- grid/doc.go
+++ grid/doc.go
@@ -3,6 +3,14 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package grid provides the main code for the Grid SVG vector drawing program
+Package grid is the top-level repository for the Grid Go SVG vector drawing program
+the GoGi GUI framework.
+
+The following sub-packages provide all the code:
+
+* grid: is the main codebase, in library form which can be
+used in different contexts.
+
+* cmd: is where the actual command tool is built.
 */
 package grid
