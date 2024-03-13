89ddeaf62eb2464bc77c97c48a3163b9356dd570
Admin <2762713521@qq.com>
2024-03-13 20:04:13 +0800

    fix ai queryModelList

--- ai/ai_test.go
+++ ai/ai_test.go
@@ -7,12 +7,12 @@ import (
 )
 
 func Test_queryModelList(t *testing.T) {
-	queryModelList(stream.NewReadFile("library.html"))
-	queryModelTags(stream.NewReadFile("tags.html")) //todo for the test we need add another tag html file
-	println(root.Format(root))                      //todo this need a treeTableView for show all tags in every model
+	queryModelList(stream.NewReadFile("library.html")) //root
+	queryModelTags(stream.NewReadFile("tags.html"))    //todo for the test we need add another tag html file
+	println(root.Format(root))                         //todo this need a treeTableView for show all tags in every model
 	//todo save n-nar model tree to json, and when need update we should read from json file
 
 	return
 	resetModels()
-	mylog.Struct(Models)
+	mylog.Struct(Models) //this is not well for show all tag,we should remove it
 }
