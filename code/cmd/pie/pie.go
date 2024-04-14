3e7173eb191a9bd0ce8202a7757346b944dbd421
Kai O'Reilly <koreilly5297@gmail.com>
2024-04-13 17:45:33 -0700

    clean up remaining goki references

--- code/cmd/pie/pie.go
+++ code/cmd/pie/pie.go
@@ -6,69 +6,66 @@ package main
 
 func main() {
 	/*
-			system.TheApp.SetName("pie")
-			system.TheApp.SetAbout(`<code>Pie</code> is the interactive parser (pi) editor written in the <b>Cogent Core</b> graphical interface system, within the <b>Goki</b> tree framework.  See <a href="https://cogentcore.org/core/parse">Code on GitHub</a> and <a href="https://cogentcore.org/core/parse/wiki">Code wiki</a> for documentation.<br>
-		<br>
-		Version: ` + pi.VersionInfo())
+		system.TheApp.SetName("pie")
 
-			// system.TheApp.SetQuitCleanFunc(func() {
-			// 	fmt.Printf("Doing final Quit cleanup here..\n")
-			// })
+		// system.TheApp.SetQuitCleanFunc(func() {
+		// 	fmt.Printf("Doing final Quit cleanup here..\n")
+		// })
 
-			code.InitSettings()
-			piv.InitSettings()
+		code.InitSettings()
+		piv.InitSettings()
 
-			code.TheConsole.Init("") // must do this after changing stdout
+		code.TheConsole.Init("") // must do this after changing stdout
 
-			// var path string
-			// var proj string
-			//
-			// // process command args
-			// if len(os.Args) > 1 {
-			// 	flag.StringVar(&path, "path", "", "path to open -- can be to a directory or a filename within the directory")
-			// 	flag.StringVar(&proj, "proj", "", "project file to open -- typically has .code extension")
-			// 	// todo: other args?
-			// 	flag.Parse()
-			// 	if path == "" && proj == "" {
-			// 		if flag.NArg() > 0 {
-			// 			ext := strings.ToLower(filepath.Ext(flag.Arg(0)))
-			// 			if ext == ".code" {
-			// 				proj = flag.Arg(0)
-			// 			} else {
-			// 				path = flag.Arg(0)
-			// 			}
-			// 		}
-			// 	}
-			// }
+		// var path string
+		// var proj string
+		//
+		// // process command args
+		// if len(os.Args) > 1 {
+		// 	flag.StringVar(&path, "path", "", "path to open -- can be to a directory or a filename within the directory")
+		// 	flag.StringVar(&proj, "proj", "", "project file to open -- typically has .code extension")
+		// 	// todo: other args?
+		// 	flag.Parse()
+		// 	if path == "" && proj == "" {
+		// 		if flag.NArg() > 0 {
+		// 			ext := strings.ToLower(filepath.Ext(flag.Arg(0)))
+		// 			if ext == ".code" {
+		// 				proj = flag.Arg(0)
+		// 			} else {
+		// 				path = flag.Arg(0)
+		// 			}
+		// 		}
+		// 	}
+		// }
 
-			recv := core.WidgetBase{}
-			recv.InitName(&recv, "pie_dummy")
+		recv := core.WidgetBase{}
+		recv.InitName(&recv, "pie_dummy")
 
-			inQuitPrompt := false
-			system.TheApp.SetQuitReqFunc(func() {
-				if !inQuitPrompt {
-					inQuitPrompt = true
-					if piv.QuitReq() {
-						system.TheApp.Quit()
-					} else {
-						inQuitPrompt = false
-					}
+		inQuitPrompt := false
+		system.TheApp.SetQuitReqFunc(func() {
+			if !inQuitPrompt {
+				inQuitPrompt = true
+				if piv.QuitReq() {
+					system.TheApp.Quit()
+				} else {
+					inQuitPrompt = false
 				}
-			})
+			}
+		})
 
-			// if proj != "" {
-			// 	proj, _ = filepath.Abs(proj)
-			// 	code.OpenCodeProject(proj)
-			// } else {
-			// 	if path != "" {
-			// 		path, _ = filepath.Abs(path)
-			// 	}
-			// 	code.NewCodeProjectPath(path)
-			// }
+		// if proj != "" {
+		// 	proj, _ = filepath.Abs(proj)
+		// 	code.OpenCodeProject(proj)
+		// } else {
+		// 	if path != "" {
+		// 		path, _ = filepath.Abs(path)
+		// 	}
+		// 	code.NewCodeProjectPath(path)
+		// }
 
-			piv.NewPiView()
+		piv.NewPiView()
 
-			// above NewCodeProject calls will have added to WinWait..
-			core.WinWait.Wait()
+		// above NewCodeProject calls will have added to WinWait..
+		core.WinWait.Wait()
 	*/
 }
