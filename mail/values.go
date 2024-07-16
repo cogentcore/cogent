971049e3e7890419628431cb58782b72987fdd1e
Kai O'Reilly <koreilly5297@gmail.com>
2024-07-16 14:44:24 -0700

    clean up in mail

--- mail/values.go
+++ mail/values.go
@@ -4,25 +4,21 @@
 
 package mail
 
-import (
-	"cogentcore.org/core/core"
-)
-
 func init() {
 	// TODO: unclear if we need a distinct type here?
 	// core.AddValue(mail.Address{}, func() core.Value { return &AddressValue{} })
 }
 
-// AddressTextField represents a [mail.Address] with a [core.TextField].
-type AddressTextField struct {
-	core.TextField
-}
+// // AddressTextField represents a [mail.Address] with a [core.TextField].
+// type AddressTextField struct {
+// 	core.TextField
+// }
 
-// AddressTextField registers [core.Chooser] as the [core.Value] widget
-// for [SplitName]
-func (av AddressTextField) Value() core.Value {
-	return core.NewTextField()
-}
+// // AddressTextField registers [core.Chooser] as the [core.Value] widget
+// // for [SplitName]
+// func (av AddressTextField) Value() core.Value {
+// 	return core.NewTextField()
+// }
 
 // func (v *AddressValue) Config() {
 // 	v.Widget.OnChange(func(e events.Event) {
