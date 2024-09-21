041ef543a186ff0f57ab075353047fab2000ff3c
Kai O'Reilly <koreilly5297@gmail.com>
2024-09-21 10:20:17 -0700

    make a backup of the old cache file in mail first for extra safety

--- mail/cache.go
+++ mail/cache.go
@@ -299,8 +299,14 @@ func (a *App) CacheUIDs(uids []imap.UID, c *imapclient.Client, email string, mai
 			// we get interrupted or have an error. We save it through a temporary
 			// file to avoid truncating it without writing it if we quit during the process.
 			// We also start the AsyncLock here so that we cannot quit from the GUI while
-			// saving the file.
+			// saving the file. We also first make a backup of the old cache file for extra
+			// safety.
 			a.AsyncLock()
+			err = os.Rename(cacheFile, cacheFile+".backup")
+			if err != nil && !errors.Is(err, fs.ErrNotExist) {
+				a.imapMu[email].Unlock()
+				return err
+			}
 			err = jsonx.Save(&cached, cacheFile+".tmp")
 			if err != nil {
 				a.imapMu[email].Unlock()
