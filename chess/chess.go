8f69c51d2bda05ac196b4c00176b158953b33f79
Kai O'Reilly <koreilly5297@gmail.com>
2025-05-25 10:10:47 -0700

    chess: start

---
+++ chess/chess.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2025, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package chess implements a chess app.
+package chess
+
+import "cogentcore.org/core/core"
+
+// Chess is the main widget of the chess app.
+type Chess struct {
+	core.Frame
+}
