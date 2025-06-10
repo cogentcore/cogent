2568c16122c22d5aaa47d070e72f71429d73bc2f
Kai O'Reilly <koreilly5297@gmail.com>
2025-06-10 08:05:28 -0700

    clock: move logic to clock package

---
+++ clock/clock.go
@@ -0,0 +1,67 @@
+// Copyright (c) 2023, Cogent Core. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package clock provides a clock app that provides customizable
+// timers, alarms, stopwatches, and world clocks.
+package clock
+
+//go:generate core generate
+
+import (
+	"time"
+
+	"cogentcore.org/core/core"
+	"cogentcore.org/core/styles"
+)
+
+type Clock struct {
+	core.Tabs
+}
+
+func (cl *Clock) Init() {
+	cl.Tabs.Init()
+}
+
+func (cl *Clock) OnAdd() {
+	cl.Tabs.OnAdd()
+	cl.clockTab()
+	cl.timerTab()
+	cl.stopwatchTab()
+	cl.alarmTab()
+}
+
+func (cl *Clock) clockTab() {
+	tab, _ := cl.NewTab("Clock")
+	tab.Styler(func(s *styles.Style) {
+		s.CenterAll()
+	})
+	date := core.NewText(tab).SetType(core.TextHeadlineMedium)
+	date.Updater(func() {
+		date.SetText(time.Now().Format("Monday, January 2"))
+	})
+	tm := core.NewText(tab).SetType(core.TextDisplayLarge)
+	tm.Updater(func() {
+		tm.SetText(time.Now().Format("3:04:05 PM"))
+	})
+	tm.Animate(func(a *core.Animation) {
+		tm.UpdateRender()
+	})
+}
+
+func (cl *Clock) timerTab() {
+	tab, _ := cl.NewTab("Timers")
+	trd := 15 * time.Minute
+	core.NewValue(&trd, "", tab)
+	core.NewButton(tab).SetText("Start")
+}
+
+func (cl *Clock) stopwatchTab() {
+	tab, _ := cl.NewTab("Stopwatches")
+	core.NewButton(tab).SetText("Start")
+}
+
+func (cl *Clock) alarmTab() {
+	tab, _ := cl.NewTab("Alarms")
+	core.NewButton(tab).SetText("Create")
+}
--- clock/clock/clock.go
+++ clock/clock/clock.go
@@ -7,60 +7,12 @@
 package main
 
 import (
-	"time"
-
+	"cogentcore.org/cogent/clock"
 	"cogentcore.org/core/core"
-	"cogentcore.org/core/styles"
 )
 
 func main() {
 	b := core.NewBody("Cogent Clock")
-
-	ts := core.NewTabs(b)
-	clock(ts)
-	timers(ts)
-	stopwatches(ts)
-	alarms(ts)
-
+	clock.NewClock(b)
 	b.RunMainWindow()
-}
-
-func clock(ts *core.Tabs) {
-	cl, _ := ts.NewTab("Clock")
-	cl.Styler(func(s *styles.Style) {
-		s.CenterAll()
-	})
-	date := core.NewText(cl).SetType(core.TextHeadlineMedium)
-	date.Updater(func() {
-		date.SetText(time.Now().Format("Monday, January 2"))
-	})
-	tm := core.NewText(cl).SetType(core.TextDisplayLarge)
-	tm.Updater(func() {
-		tm.SetText(time.Now().Format("3:04:05 PM"))
-	})
-	tm.Animate(func(a *core.Animation) {
-		tm.UpdateRender()
-	})
-}
-
-type timer struct {
-	left time.Time
-}
-
-func timers(ts *core.Tabs) {
-	tr, _ := ts.NewTab("Timers")
-	trd := 15 * time.Minute
-	trv := core.NewValue(&trd, "")
-	tr.AddChild(trv)
-	core.NewButton(tr).SetText("Start")
-}
-
-func stopwatches(ts *core.Tabs) {
-	sw, _ := ts.NewTab("Stopwatches")
-	core.NewButton(sw).SetText("Start")
-}
-
-func alarms(ts *core.Tabs) {
-	al, _ := ts.NewTab("Alarms")
-	core.NewButton(al).SetText("Create")
 }
